b21744f8eac5cda1470fa08d88c89cf84b2780d8
Krynio <przekwaskrzysiek@gmail.com>
2017-12-28 01:54:57 +0100

    Switched to EmguCV and added facial recognition checkbox

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -7,18 +7,27 @@ using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 using WebEye.Controls.Wpf;
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+using System.Windows.Forms.Integration;
+
+
 
 namespace FaceTracker
 {
     public partial class MainWindow
     {
+        private Capture _capture;
 
-        WebCameraControl webCameraControl = new WebCameraControl();
+        private CascadeClassifier _cascadeClassifier;
+
         public MainWindow()
         {
             InitializeComponent();
-            InitializeComboBox();
+            _capture = new Capture();
 
+            _cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
             DispatcherTimer timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromMilliseconds(33);
             timer.Tick += timer_Tick;
@@ -27,56 +36,31 @@ namespace FaceTracker
 
         private void timer_Tick(object sender, EventArgs e)
         {
-            if (webCameraControl.IsCapturing)
+            if (FaceDetectionCheckBox.IsChecked.Value)
             {
-                WebCameraView.Source = Convert(webCameraControl.GetCurrentImage());
+
+                using (var imageFrame = _capture.QueryFrame().ToImage<Bgr, byte>())
+                {
+                    if (imageFrame != null)
+                    {
+                        var grayframe = imageFrame.Convert<Gray, byte>();
+                        var faces = _cascadeClassifier.DetectMultiScale(grayframe, 1.1, 10, System.Drawing.Size.Empty); //the actual face detection happens here
+                        foreach (var face in faces)
+                        {
+                            imageFrame.Draw(face, new Bgr(Color.BurlyWood), 3); //the detected face(s) is highlighted here using a box that is drawn around it/them
+                           
+                        }
+                    }
+                    WebCameraView.Image = imageFrame;                    
+                }
+                
+            }
+            else
+            {
+                WebCameraView.Image = _capture.QueryFrame();
             }
         }
-
-        private void InitializeComboBox()
-        {
-            ComboBox.ItemsSource = webCameraControl.GetVideoCaptureDevices();
-
-            if (ComboBox.Items.Count > 0)
-            {
-                ComboBox.SelectedItem = ComboBox.Items[0];
-            }
-        }
-
-        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            StartButton.IsEnabled = e.AddedItems.Count > 0;
-        }
-
-        private void OnStartButtonClick(object sender, RoutedEventArgs e)
-        {
-            var cameraId = (WebCameraId)ComboBox.SelectedItem;
-            webCameraControl.StartCapture(cameraId);
-
-            StopButton.IsEnabled = true;
-            ImageButton.IsEnabled = true;
-        }
-
-        private void OnStopButtonClick(object sender, RoutedEventArgs e)
-        {
-            webCameraControl.StopCapture();
-        }
-
-        private void OnImageButtonClick(object sender, RoutedEventArgs e)
-        {
-            WebCameraView.Source = Convert(webCameraControl.GetCurrentImage());
-        }
-
-        public BitmapImage Convert(Bitmap src)
-        {
-            MemoryStream ms = new MemoryStream();
-            src.Save(ms, ImageFormat.Bmp);
-            BitmapImage image = new BitmapImage();
-            image.BeginInit();
-            ms.Seek(0, SeekOrigin.Begin);
-            image.StreamSource = ms;
-            image.EndInit();
-            return image;
-        }
+        
+        
     }
 }
