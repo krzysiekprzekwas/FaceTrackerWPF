0c597e92fc6bf9ab602566b7a07b02bfcc0871fd
Krynio <przekwaskrzysiek@gmail.com>
2018-01-04 13:33:09 +0100

    Fixed OutOfMemory bug related to BitmapConverter

--- WpfApplication1/BitmapToBitmapSourceConverter.cs
+++ WpfApplication1/BitmapToBitmapSourceConverter.cs
@@ -4,24 +4,33 @@ using System.Drawing;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
+using System.Windows.Interop;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace FaceTracker
 {
     class BitmapToBitmapSourceConverter : IValueConverter
     {
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteObject([In] IntPtr hObject);
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var bitmap = value as Bitmap;
 
-            return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
-                                 bitmap.GetHbitmap(),
-                                 IntPtr.Zero,
-                                 System.Windows.Int32Rect.Empty,
-                                 BitmapSizeOptions.FromWidthAndHeight(bitmap.Width, bitmap.Height));
+            var handle = bitmap.GetHbitmap();
+            try
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally { DeleteObject(handle); }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
