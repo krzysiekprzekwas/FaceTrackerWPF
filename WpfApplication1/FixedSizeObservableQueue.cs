e5c084b184f46fc2b395a61216c5bacc623899cc
Krynio <przekwaskrzysiek@gmail.com>
2018-01-07 00:16:17 +0100

    Changed ObservableQueue to fixed size

--- WpfApplication1/FixedSizeObservableQueue.cs
+++ WpfApplication1/FixedSizeObservableQueue.cs
@@ -5,14 +5,24 @@ using System.Collections.Specialized;
 
 namespace FaceTracker
 {
-    public class ObservableQueue<T> : INotifyCollectionChanged, IEnumerable<T>
+    public class FixedSizeObservableQueue<T> : INotifyCollectionChanged, IEnumerable<T>
     {
+        public FixedSizeObservableQueue(int limit)
+        {
+            Limit = limit;
+        }
+        public int Limit { get; set; }
+
         public event NotifyCollectionChangedEventHandler CollectionChanged;
         private readonly Queue<T> _queue = new Queue<T>();
 
         public void Enqueue(T item)
         {
             _queue.Enqueue(item);
+
+            if (_queue.Count > Limit)
+                Dequeue();
+
             CollectionChanged?.Invoke(this,
                 new NotifyCollectionChangedEventArgs(
                     NotifyCollectionChangedAction.Add, item));
