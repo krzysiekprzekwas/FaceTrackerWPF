525532be8f40a96dda2e6e5da764cf7ea9f0848f
Krynio <przekwaskrzysiek@gmail.com>
2018-01-17 19:12:09 +0100

    Created simple QualityEnum to String converter

---
+++ WpfApplication1/QualityEnumToStringValueConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace FaceTracker
+{
+    class QualityEnumToStringValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            QualityEnum quality = (QualityEnum)(value);
+            return quality.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
